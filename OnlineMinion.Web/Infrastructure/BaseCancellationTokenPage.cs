f03d3deb4997739baf62d9e73044bc4c016bd3a9
Lauri Saar <lauri.saar@outlook.com>
2023-05-10 15:03:21 +0100

    REFACTOR(AccountSpecsPage.razor): moved `CancellationTokenSource` handling into baseclass:
    - along the way, renamed page components to have "Page" suffix.

---
+++ OnlineMinion.Web/Infrastructure/BaseCancellationTokenPage.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components;
+
+namespace OnlineMinion.Web.Infrastructure;
+
+public abstract class BaseCancellationTokenPage : ComponentBase, IAsyncDisposable
+{
+    protected readonly CancellationTokenSource CancellationTokenSource;
+    private bool _disposed;
+
+    protected BaseCancellationTokenPage() => CancellationTokenSource = new();
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (!_disposed)
+        {
+            _disposed = true;
+            await CancellationTokenSource.CancelAsync();
+            CancellationTokenSource.Dispose();
+        }
+
+        GC.SuppressFinalize(this);
+    }
+}
