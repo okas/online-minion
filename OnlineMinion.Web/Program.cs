c2e5c7239d38e30c02098accd7feb6e7e0ea4266
Lauri Saar <lauri.saar@outlook.com>
2023-05-17 19:35:31 +0100

    CHANGE(Web.Program): file layout & added proper logging configuration
    - after revert!

--- OnlineMinion.Web/Program.cs
+++ OnlineMinion.Web/Program.cs
@@ -6,33 +6,41 @@ using OnlineMinion.Web.Settings;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-//-- Configurations setup
-builder.Services.Configure<ApiClientSettings>(builder.Configuration.GetSection(nameof(ApiClientSettings)));
+#region Container setup
 
-// Services setup
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging")
+);
+
 builder.Services.AddHttpClient(
     Constants.HostClient,
     client => client.BaseAddress = new(builder.HostEnvironment.BaseAddress)
 );
 
-builder.Services.AddHttpClient(
-    Constants.ApiClient,
-    (provider, client) =>
-        client.BaseAddress = new(
-            provider.GetService<IOptions<ApiClientSettings>>()?.Value.Url ??
-            throw new InvalidOperationException($"Missing {nameof(ApiClientSettings)} from configuration.")
-        )
-);
+builder.Services
+    .Configure<ApiClientSettings>(builder.Configuration.GetSection(nameof(ApiClientSettings)))
+    .AddHttpClient(
+        Constants.ApiClient,
+        (provider, client) =>
+            client.BaseAddress = new(
+                provider.GetService<IOptions<ApiClientSettings>>()?.Value.Url ??
+                throw new InvalidOperationException($"Missing {nameof(ApiClientSettings)} from configuration.")
+            )
+    );
 
 builder.Services.AddMediatR(
     opts => opts.RegisterServicesFromAssemblyContaining<Program>()
 );
 
-//--
+#endregion
+
+#region Components setup
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//--
+#endregion
+
 var webAssemblyHost = builder.Build();
 
-await webAssemblyHost.RunAsync();
+await webAssemblyHost.RunAsync().ConfigureAwait(false);
