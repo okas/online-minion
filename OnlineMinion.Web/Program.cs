c60f8c52fcd9bbcb2db269081ddaae4f8e32c821
Lauri Saar <lauri.saar@outlook.com>
2023-05-17 18:41:16 +0100

    CHANGE(solution): upgraded to .NET8 preview 4 & implemented some new features along the way

--- OnlineMinion.Web/Program.cs
+++ OnlineMinion.Web/Program.cs
@@ -6,23 +6,27 @@ using OnlineMinion.Web.Settings;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-//-- Configurations setup
-builder.Services.Configure<ApiClientSettings>(builder.Configuration.GetSection(nameof(ApiClientSettings)));
+#region Container setup
 
-// Services setup
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging")
+);
+
 builder.Services.AddHttpClient(
     Constants.ApiClient,
     client => client.BaseAddress = new(builder.HostEnvironment.BaseAddress)
 );
 
-builder.Services.AddHttpClient(
-    Constants.ApiClient,
-    (provider, client) =>
-        client.BaseAddress = new(
-            provider.GetService<IOptions<ApiClientSettings>>()?.Value.Url ??
-            throw new InvalidOperationException($"Missing {nameof(ApiClientSettings)} from configuration.")
-        )
-);
+builder.Services
+    .Configure<ApiClientSettings>(builder.Configuration.GetSection(nameof(ApiClientSettings)))
+    .AddHttpClient(
+        Constants.ApiClient,
+        (provider, client) =>
+            client.BaseAddress = new(
+                provider.GetService<IOptions<ApiClientSettings>>()?.Value.Url ??
+                throw new InvalidOperationException($"Missing {nameof(ApiClientSettings)} from configuration.")
+            )
+    );
 
 builder.Services.AddMediatR(
     opts => opts.RegisterServicesFromAssemblyContaining<Program>()
@@ -32,7 +36,8 @@ builder.Services.AddMediatR(
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//--
+#endregion
+
 var webAssemblyHost = builder.Build();
 
-await webAssemblyHost.RunAsync();
+await webAssemblyHost.RunAsync().ConfigureAwait(false);
