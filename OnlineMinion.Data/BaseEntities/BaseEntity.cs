48ec80551ceb207a991d7ad990f1cb09e08d8d93
Lauri Saar <lauri.saar@outlook.com>
2023-05-20 23:20:00 +0100

    CHANGE(OnlineMinion.Data.BaseEntities.BaseEntity): moved to Data assembly, not supposed to be available outside of "backend"
    CHANGE(OnlineMinion.RestApi.AppMessaging.Requests.GetPagingMetaInfoReq): moved to RestApi assembly, not supposed to be used outside of assembly

---
+++ OnlineMinion.Data/BaseEntities/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace OnlineMinion.Data.BaseEntities;
+
+// TODO: move to Data assembly, because should not be visible outside of "backend"
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+}
