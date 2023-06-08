d4666ff62f90813488dceeb0b4f58c82be3a61db
Lauri Saar <lauri.saar@outlook.com>
2023-06-08 13:02:52 +0100

    ADD(Add_HiLo-based_PK_generation): adds support for application to know db id-s before creating them in db:
    - NB! DOWN method is merely a draft, data is not migrated with this.

--- OnlineMinion.Data/Migrations/20230607225221_Add_HiLobased_PK_generation.Designer.cs
+++ OnlineMinion.Data/Migrations/20230607225221_Add_HiLobased_PK_generation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineMinion.Data;
 
@@ -11,9 +12,11 @@ using OnlineMinion.Data;
 namespace OnlineMinion.Data.Migrations
 {
     [DbContext(typeof(OnlineMinionDbContext))]
-    partial class OnlineMinionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230607225221_Add_HiLobased_PK_generation")]
+    partial class Add_HiLobased_PK_generation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
