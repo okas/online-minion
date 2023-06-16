d3cdbbc6ad04796fddd3aefe2067e7c2e0cb59ab
Lauri Saar <lauri.saar@outlook.com>
2023-06-16 17:50:21 +0100

    FIX(AccountSpec create & update validator): they have different uniqueness conditions:
    - removed "base" class of these validators, because it was ill-fitted.

--- OnlineMinion.RestApi/Validation/CreateAccountSpecUniqueNameValidator.cs
+++ OnlineMinion.RestApi/Validation/CreateAccountSpecUniqueNameValidator.cs
@@ -1,11 +1,26 @@
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using OnlineMinion.Contracts.AppMessaging.Requests;
+using OnlineMinion.Data;
 
 namespace OnlineMinion.RestApi.Validation;
 
 public sealed class CreateAccountSpecUniqueNameValidator : AbstractValidator<CreateAccountSpecReq>,
     IAsyncUniqueValidator<CreateAccountSpecReq>
 {
-    public CreateAccountSpecUniqueNameValidator(BaseUpsertAccountSpecUniqueNameValidator baseValidator) =>
-        Include(baseValidator);
+    private readonly OnlineMinionDbContext _dbContext;
+
+    public CreateAccountSpecUniqueNameValidator(OnlineMinionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+
+        RuleFor(x => x.Name)
+            .MustAsync(BeUnique)
+            .WithMessage("'{PropertyName}' must be unique");
+    }
+
+    private async Task<bool> BeUnique(CreateAccountSpecReq req, string name, CancellationToken ct) =>
+        await _dbContext.AccountSpecs
+            .AllAsync(entity => entity.Name != name, ct)
+            .ConfigureAwait(false);
 }
--- OnlineMinion.RestApi/Validation/UpdateAccountSpecUniqueNameValidator.cs
+++ OnlineMinion.RestApi/Validation/UpdateAccountSpecUniqueNameValidator.cs
@@ -1,11 +1,26 @@
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using OnlineMinion.Contracts.AppMessaging.Requests;
+using OnlineMinion.Data;
 
 namespace OnlineMinion.RestApi.Validation;
 
 public sealed class UpdateAccountSpecUniqueNameValidator : AbstractValidator<UpdateAccountSpecReq>,
     IAsyncUniqueValidator<UpdateAccountSpecReq>
 {
-    public UpdateAccountSpecUniqueNameValidator(BaseUpsertAccountSpecUniqueNameValidator baseValidator) =>
-        Include(baseValidator);
+    private readonly OnlineMinionDbContext _dbContext;
+
+    public UpdateAccountSpecUniqueNameValidator(OnlineMinionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+
+        RuleFor(x => x.Name)
+            .MustAsync(BeUnique)
+            .WithMessage("'{PropertyName}' must be unique");
+    }
+
+    private async Task<bool> BeUnique(UpdateAccountSpecReq req, string name, CancellationToken ct) =>
+        !await _dbContext.AccountSpecs
+            .AnyAsync(entity => entity.Id != req.Id && entity.Name == name, ct)
+            .ConfigureAwait(false);
 }
