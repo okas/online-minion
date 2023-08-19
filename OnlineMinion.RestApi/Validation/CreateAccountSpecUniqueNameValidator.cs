7836d7ec75db8b36f582a7510ffcfa734ed91197
Lauri Saar <lauri.saar@outlook.com>
2023-08-20 00:59:32 +0100

    REFACTOR(`BaseCreateModelUniqueNameValidator`): pulled common code into new base class

--- OnlineMinion.RestApi/Validation/CreateAccountSpecUniqueNameValidator.cs
+++ OnlineMinion.RestApi/Validation/CreateAccountSpecUniqueNameValidator.cs
@@ -1,25 +1,18 @@
 using FluentValidation;
 using MediatR;
-using OnlineMinion.Common;
 using OnlineMinion.Contracts.AppMessaging.Requests;
 
 namespace OnlineMinion.RestApi.Validation;
 
-public sealed class CreateAccountSpecUniqueNameValidator : AbstractValidator<CreateAccountSpecReq>,
-    IAsyncUniqueValidator<CreateAccountSpecReq>
+public sealed class CreateAccountSpecUniqueNameValidator : BaseCreateModelUniqueNameValidator<CreateAccountSpecReq>
 {
-    private readonly ISender _sender;
-
-    public CreateAccountSpecUniqueNameValidator(ISender sender)
+    public CreateAccountSpecUniqueNameValidator(ISender sender) : base(sender)
     {
-        _sender = sender;
-
         RuleFor(x => x.Name)
             .MustAsync(BeUnique)
-            .WithMessage("'{PropertyName}' must be unique");
+            .WithMessage(FailureMessageFormat);
     }
 
-    // TODO: To interface IAsyncUniqueValidator<TModel>  member?
-    private async Task<bool> BeUnique(string name, CancellationToken ct) =>
-        await _sender.Send(new CheckAccountSpecUniqueNewReq(name), ct).ConfigureAwait(false);
+    protected override IRequest<bool> ValidationRequestFactory(CreateAccountSpecReq model, string value) =>
+        new CheckAccountSpecUniqueNewReq(value);
 }
--- OnlineMinion.RestApi/Validation/UpdateAccountSpecUniqueNameValidator.cs
+++ OnlineMinion.RestApi/Validation/UpdateAccountSpecUniqueNameValidator.cs
@@ -1,25 +1,18 @@
 using FluentValidation;
 using MediatR;
-using OnlineMinion.Common;
 using OnlineMinion.Contracts.AppMessaging.Requests;
 
 namespace OnlineMinion.RestApi.Validation;
 
-public sealed class UpdateAccountSpecUniqueNameValidator : AbstractValidator<UpdateAccountSpecReq>,
-    IAsyncUniqueValidator<UpdateAccountSpecReq>
+public sealed class UpdateAccountSpecUniqueNameValidator : BaseCreateModelUniqueNameValidator<UpdateAccountSpecReq>
 {
-    private readonly ISender _sender;
-
-    public UpdateAccountSpecUniqueNameValidator(ISender sender)
+    public UpdateAccountSpecUniqueNameValidator(ISender sender) : base(sender)
     {
-        _sender = sender;
-
         RuleFor(x => x.Name)
             .MustAsync(BeUnique)
-            .WithMessage("'{PropertyName}' must be unique");
+            .WithMessage(FailureMessageFormat);
     }
 
-    // TODO: To interface IAsyncUniqueValidator<TModel>  member?
-    private async Task<bool> BeUnique(UpdateAccountSpecReq req, string name, CancellationToken ct) =>
-        await _sender.Send(new CheckAccountSpecUniqueExistingReq(name, req.Id), ct).ConfigureAwait(false);
+    protected override IRequest<bool> ValidationRequestFactory(UpdateAccountSpecReq model, string value) =>
+        new CheckAccountSpecUniqueExistingReq(value, model.Id);
 }
