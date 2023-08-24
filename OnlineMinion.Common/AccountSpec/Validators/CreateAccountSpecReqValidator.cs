2e9713f7b5d3a59b29c479693ae6df5e0d655d2f
Lauri Saar <lauri.saar@outlook.com>
2023-08-24 21:15:25 +0100

    REFACTOR([UsedImplicitly]): added missing attributes

--- OnlineMinion.Common/AccountSpec/Validators/CreateAccountSpecReqValidator.cs
+++ OnlineMinion.Common/AccountSpec/Validators/CreateAccountSpecReqValidator.cs
@@ -1,8 +1,10 @@
 using FluentValidation;
+using JetBrains.Annotations;
 using OnlineMinion.Contracts.AccountSpec.Requests;
 
 namespace OnlineMinion.Common.AccountSpec.Validators;
 
+[UsedImplicitly]
 public class CreateAccountSpecReqValidator : AbstractValidator<CreateAccountSpecReq>
 {
     public CreateAccountSpecReqValidator(BaseUpsertAccountSpecReqDataValidator baseValidator) =>
--- OnlineMinion.Common/AccountSpec/Validators/UpdateAccountSpecReqValidator.cs
+++ OnlineMinion.Common/AccountSpec/Validators/UpdateAccountSpecReqValidator.cs
@@ -1,9 +1,11 @@
 using FluentValidation;
+using JetBrains.Annotations;
 using OnlineMinion.Common.Common.Validation;
 using OnlineMinion.Contracts.AccountSpec.Requests;
 
 namespace OnlineMinion.Common.AccountSpec.Validators;
 
+[UsedImplicitly]
 public class UpdateAccountSpecReqValidator : AbstractValidator<UpdateAccountSpecReq>
 {
     public UpdateAccountSpecReqValidator(
