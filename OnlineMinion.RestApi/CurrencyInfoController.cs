f951b4fceadd97fb10ae4bb8f352795446b8c43d
Lauri Saar <lauri.saar@outlook.com>
2023-09-07 00:10:38 +0100

    FIX(`CurrencyInfoController.GetCurrencies`): OAS3 incomplete, added return type info

--- OnlineMinion.RestApi/CurrencyInfoController.cs
+++ OnlineMinion.RestApi/CurrencyInfoController.cs
@@ -1,6 +1,8 @@
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OnlineMinion.Contracts.CurrencyInfo.Requests;
+using OnlineMinion.Contracts.CurrencyInfo.Responses;
 using OnlineMinion.RestApi.BaseControllers;
 
 namespace OnlineMinion.RestApi;
@@ -9,6 +11,7 @@ namespace OnlineMinion.RestApi;
 public class CurrencyInfoController(ISender sender) : BaseApiController
 {
     [HttpGet]
+    [ProducesResponseType(typeof(IAsyncEnumerable<CurrencyInfoResp>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetCurrencies(CancellationToken ct)
     {
         var rq = new GetCurrenciesReq();
