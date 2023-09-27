daa6dffea92a54e8f4943f2fdedf601c935a5573
Lauri Saar <lauri.saar@outlook.com>
2023-09-27 06:25:43 +0100

    CHANGE(`Application.Program`): Swagger Gen is now permanent system component, so that OAS doc can be retrieved anytime:
    - Swagger UI remain as a dev only feature;
    - also reorganized bootstrapping code for clarity.

--- OnlineMinion.Application/Program.cs
+++ OnlineMinion.Application/Program.cs
@@ -7,47 +7,47 @@ using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
+const string connectionString = "name=ConnectionStrings:DefaultConnection";
+
 var webAppBuilder = WebApplication.CreateBuilder(args);
 
 #region Container setup
 
 var confManager = webAppBuilder.Configuration;
-
 var services = webAppBuilder.Services;
 
 webAppBuilder.Services.AddDbContext<OnlineMinionDbContext>(
-    builder => builder.UseSqlServer("name=ConnectionStrings:DefaultConnection")
+    builder => builder.UseSqlServer(connectionString)
 );
 
 services.AddRestApi(webAppBuilder.Configuration);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.Configure<SwaggerOptions>(confManager.GetSection(nameof(SwaggerOptions)));
+
+services
+    .Configure<SwaggerGeneratorOptions>(confManager.GetSection(nameof(SwaggerGeneratorOptions)))
+    .Configure<SchemaGeneratorOptions>(confManager.GetSection(nameof(SchemaGeneratorOptions)))
+    .Configure<SwaggerGenOptions>(confManager.GetSection(nameof(SwaggerGenOptions)))
+    .ConfigureOptions<SwaggerGenOptionsConfigurator>()
+    .AddSwaggerGen()
+    .AddFluentValidationRulesToSwagger(
+        options => options.ValidatorSearch = new()
+        {
+            IsOneValidatorForType = false,
+            SearchBaseTypeValidators =
+                false, // This aligns with the behavior of default MS DI  Service resolution works!
+            // Enforces to use only one validator per type and combine them using 'Include' rules.
+        }
+    );
 
 if (webAppBuilder.Environment.IsDevelopment())
 {
     services.AddDatabaseDeveloperPageExceptionFilter();
 
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    services.Configure<SwaggerOptions>(confManager.GetSection(nameof(SwaggerOptions)));
-
-    services
-        .Configure<SwaggerGeneratorOptions>(confManager.GetSection(nameof(SwaggerGeneratorOptions)))
-        .Configure<SchemaGeneratorOptions>(confManager.GetSection(nameof(SchemaGeneratorOptions)))
-        .Configure<SwaggerGenOptions>(confManager.GetSection(nameof(SwaggerGenOptions)))
-        .ConfigureOptions<SwaggerGenOptionsConfigurator>()
-        .AddFluentValidationRulesToSwagger(
-            options => options.ValidatorSearch = new()
-            {
-                IsOneValidatorForType = false,
-                SearchBaseTypeValidators =
-                    false, // This aligns with the behavior of default MS DI  Service resolution works!
-                // Enforces to use only one validator per type and combine them using 'Include' rules.
-            }
-        );
-
     services
         .Configure<SwaggerUIOptions>(confManager.GetSection(nameof(SwaggerUIOptions)))
         .ConfigureOptions<SwaggerUIOptionsConfigurator>();
-
-    services.AddSwaggerGen();
 }
 
 #endregion
@@ -56,10 +56,10 @@ var app = webAppBuilder.Build();
 
 #region HTTP request pipeline
 
+app.UseSwagger();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-
     // Required to serve custom CSS for SwaggerUI.
     app.UseStaticFiles();
 
