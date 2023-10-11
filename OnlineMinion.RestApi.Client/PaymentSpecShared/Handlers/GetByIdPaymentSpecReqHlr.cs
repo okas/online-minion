c49609348458afc210a7341aea352c534ae0bcff
Lauri Saar <lauri.saar@outlook.com>
2023-10-11 20:42:59 +0100

    CHANGE('OnlineMinion.RestApi.Client.PaymentSpecShared.Handlers.GetByIdPaymentSpecReqHlr'): capability to deserialize responses to concrete types, using `content-type` header value:
    - this way there isn't duplicated API endpoints for very similar results, basically subtypes of entity;
    - `OnlineMinion.RestApi.Client.Shared.Handlers.BaseGetModelByIdReqHlr<TRequest,TResponse>` has now strategy  pattern usage to delegate response deserializing special cases to base handler's implementor;
    - this method is virtual, with default behavior of deserializing TResponse as default.

--- OnlineMinion.RestApi.Client/PaymentSpecShared/Handlers/GetByIdPaymentSpecReqHlr.cs
+++ OnlineMinion.RestApi.Client/PaymentSpecShared/Handlers/GetByIdPaymentSpecReqHlr.cs
@@ -1,4 +1,6 @@
+using System.Net.Http.Json;
 using JetBrains.Annotations;
+using OnlineMinion.Application.Contracts;
 using OnlineMinion.Application.Contracts.PaymentSpecShared.Requests;
 using OnlineMinion.Application.Contracts.PaymentSpecShared.Responses;
 using OnlineMinion.RestApi.Client.Api;
@@ -9,4 +11,26 @@ namespace OnlineMinion.RestApi.Client.PaymentSpecShared.Handlers;
 
 [UsedImplicitly]
 internal sealed class GetByIdPaymentSpecReqHlr(ApiProvider api)
-    : BaseGetModelByIdReqHlr<GetByIdPaymentSpecReq, BasePaymentSpecResp>(api.Client, ApiPaymentSpecsUri);
+    : BaseGetModelByIdReqHlr<GetByIdPaymentSpecReq, BasePaymentSpecResp>(api.Client, ApiPaymentSpecsUri)
+{
+    protected override async ValueTask<BasePaymentSpecResp?> ToResponse(
+        HttpResponseMessage response,
+        CancellationToken   ct
+    )
+    {
+        var concreteType = response.Content.Headers.ContentType?.MediaType switch
+        {
+            CustomVendorContentTypes.PaymentSpecCashJson => typeof(PaymentSpecCashResp),
+            CustomVendorContentTypes.PaymentSpecBankJson => typeof(PaymentSpecBankResp),
+            CustomVendorContentTypes.PaymentSpecCryptoJson => typeof(PaymentSpecCryptoResp),
+            _ => typeof(void),
+        };
+
+        if (concreteType == typeof(void))
+        {
+            return null;
+        }
+
+        return await response.Content.ReadFromJsonAsync(concreteType, ct).ConfigureAwait(false) as BasePaymentSpecResp;
+    }
+}
--- OnlineMinion.RestApi.Client/Shared/Handlers/BaseGetModelByIdReqHlr.cs
+++ OnlineMinion.RestApi.Client/Shared/Handlers/BaseGetModelByIdReqHlr.cs
@@ -9,13 +9,20 @@ internal abstract class BaseGetModelByIdReqHlr<TRequest, TResponse>(HttpClient a
     : IApiClientRequestHandler<TRequest, TResponse>
     where TRequest : IGetByIdRequest<TResponse>
 {
+    // ReSharper disable once StaticMemberInGenericType
+    private static readonly string MsgEmptyContent = $"Server returned {(int)HttpStatusCode.OK} but response is empty.";
+
+    // ReSharper disable once StaticMemberInGenericType
+    private static readonly string MsgUnknownContent = $"Server returned content with response '{(int)HttpStatusCode.OK
+    }', but header `Content-Type` is unknown, unable to serialize.";
+
     public async Task<ErrorOr<TResponse>> Handle(TRequest rq, CancellationToken ct)
     {
         var uri = BuildUri(rq);
 
         var responseMessage = await apiClient.GetAsync(uri, ct).ConfigureAwait(false);
 
-        return await HandleResponse(responseMessage).ConfigureAwait(false);
+        return await HandleResponse(responseMessage, ct).ConfigureAwait(false);
     }
 
     public virtual Uri BuildUri(TRequest rq) => new(
@@ -23,20 +30,33 @@ internal abstract class BaseGetModelByIdReqHlr<TRequest, TResponse>(HttpClient a
         UriKind.RelativeOrAbsolute
     );
 
-    private static async ValueTask<ErrorOr<TResponse>> HandleResponse(HttpResponseMessage response) =>
-        response.StatusCode switch
-        {
-            HttpStatusCode.OK => await HandleExpectedHttpResult(response).ConfigureAwait(false),
-            _ => Error.Failure(description: $"Server returned {response.StatusCode}"),
-        };
-
-    private static async ValueTask<ErrorOr<TResponse>> HandleExpectedHttpResult(HttpResponseMessage response)
+    private async ValueTask<ErrorOr<TResponse>> HandleResponse(
+        HttpResponseMessage response,
+        CancellationToken   ct
+    ) => response switch
     {
-        if (await response.Content.ReadFromJsonAsync<TResponse>().ConfigureAwait(false) is { } result)
+        { StatusCode: HttpStatusCode.OK, Content.Headers.ContentLength: > 0, } =>
+            await HandleExpectedHttpResult(response, ct).ConfigureAwait(false),
+
+        { StatusCode: HttpStatusCode.OK, Content.Headers.ContentLength: 0, } =>
+            Error.Unexpected(description: MsgEmptyContent),
+
+        _ => Error.Failure(description: $"Server returned {response.StatusCode}"),
+    };
+
+    private async ValueTask<ErrorOr<TResponse>> HandleExpectedHttpResult(
+        HttpResponseMessage response,
+        CancellationToken   ct
+    )
+    {
+        if (await ToResponse(response, ct).ConfigureAwait(false) is { } result)
         {
             return result;
         }
 
-        return Error.Unexpected(description: $"Server returned {HttpStatusCode.OK} but response is empty");
+        return Error.Failure(description: MsgUnknownContent);
     }
+
+    protected virtual ValueTask<TResponse?> ToResponse(HttpResponseMessage response, CancellationToken ct) =>
+        new(response.Content.ReadFromJsonAsync<TResponse>(ct));
 }
